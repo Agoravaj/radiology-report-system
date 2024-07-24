c6bcca6bc1912e4e1512a0c8c6cbc5db755959be
laudaj <you@example.com>
2024-07-24 11:55:15 -0300

    Fixed package.json and updated config files

--- radiology-report-system/tailwind.config.js
+++ radiology-report-system/tailwind.config.js
@@ -3,16 +3,12 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-<<<<<<< HEAD
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-=======
-  content: ["pages/**/*.{js,jsx}", "components/**/*.{js,jsx}"],
->>>>>>> f824d4077d5c473c3bda2f9741cb43fe6995ca94
+    './pages/**/*.{ts,tsx,js,jsx}',
+    './components/**/*.{ts,tsx,js,jsx}',
+    './app/**/*.{ts,tsx,js,jsx}',
+    './src/**/*.{ts,tsx,js,jsx}',
+  ],
   theme: {
     container: {
       center: true,
@@ -61,7 +57,6 @@ module.exports = {
         },
       },
       borderRadius: {
-<<<<<<< HEAD
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
@@ -79,17 +74,8 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-=======
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: "calc(var(--radius) - 4px)",
->>>>>>> f824d4077d5c473c3bda2f9741cb43fe6995ca94
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> f824d4077d5c473c3bda2f9741cb43fe6995ca94
